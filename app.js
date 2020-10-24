c3f2289583a140f311b8a28733b48b487ca2051e
frederick-bruce <69643077+frederick-bruce@users.noreply.github.com>
2020-10-24 12:39:11 -0400

    update app.js
    Added function that Loops through an array of characters and appends them to the #phrase ul as list items

--- app.js
+++ app.js
@@ -20,3 +20,28 @@ const phrases = [
 ];
 
 
+// Functions (Global)
+
+// Randomly chooses phrase from phrases array, returns a new array of characters from tge randomly chosen phrase
+function getRandomPhraseArray ( arr ) {
+  let randomPhrase =arr[ Math.floor(Math.random() * arr.length ) ];
+  let phraseArray = randomPhrase.split("");
+  return phraseArray;
+} 
+
+// Loops through an array of characters and appends them to the #phrase ul as list items
+function addPhraseToDisplay( arr ) {
+  for ( let i = 0; i < arr.length; i++) {
+    const listItem =document.createElement("li");
+    listItem.textContent = arr[i];
+    phraseUL.append(listItem);
+    // if the list has text content it isit is given the class "letter", if not, it is given the class "space"
+    if ( listItem.textContent !== " " ) {
+      listItem.className = "letter";
+    } else {
+      listItem.className = "space";
+    }
+  
+  }
+
+}
