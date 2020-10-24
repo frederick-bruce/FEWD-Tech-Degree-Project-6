0e00fc27c8f422fd0a1cc266b048d7d28449f7f6
frederick-bruce <69643077+frederick-bruce@users.noreply.github.com>
2020-10-24 11:21:12 -0400

    setup
    added files
    declared variables

---
+++ app.js
@@ -0,0 +1,22 @@
+// Declare Variables (Global)
+
+const phraseUL = document.querySelector("phrase");
+const keyBoard = document.querySelector("qwerty");
+const resetBtn = document.querySelector("btn_reset");
+const subTitle = document.querySelector(".subTitle");
+const scoreboard = document.querySelector("#scoreboard");
+const hearts = document.querySelectorAll("#scoreboard");
+const overlay = document.getElementById("overlay");
+let missed = 0;
+
+const phrases = [
+  "Javascript",
+  "Once upon a time",
+  "Video Games",
+  "The World We Live In",
+  "Programming",
+  "Dream Job",
+  "Programming"
+];
+
+
